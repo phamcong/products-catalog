cb79462f1df19551546217206d4cc1f7d552af21
Cong Cuong PHAM <ccuong.ph@gmail.com>
2018-06-10 08:41:54 +0200

    update respo

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -27,7 +27,7 @@ export class ProductsComponent implements OnInit {
   }
 
   public getJSON(): Observable<any> {
-    return this.http.get('assets/data/products.json');
+    return this.http.get('https://www.utc.fr/~phamcong/products.json');
   }
 
   private getProductsCategoryDict(products: any[]): void {
